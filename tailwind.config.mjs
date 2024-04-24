2a9d10a4183aaf1de9f05f55ca78bedac0fbb803
Abhay <abhaypratap41234@gmail.com>
2024-04-24 12:01:27 +0530

    Exam Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		// Define custom utilities
+		// Use the "::-webkit-scrollbar" selector to target the webkit scrollbar
+		// Set "display: none" to hide the scrollbar
+		// Note: This class may not hide the scrollbar in all cases, depending on the browser and scrollbar styling
+		scrollbar: {
+		  hide: {
+			'::-webkit-scrollbar': {
+			  display: 'none',
+			},
+		  },
+		},
+	  },
 	},
 	plugins: [],
-}
+  };
+  
